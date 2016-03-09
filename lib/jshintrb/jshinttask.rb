9619ecacc6ca427f4c6b9748c80d0e4c8f17e217
Tom Nadolny <tomek.nad@gmail.com>
2016-03-09 11:56:43 +0000

    making this change backwards-compatible

--- lib/jshintrb/jshinttask.rb
+++ lib/jshintrb/jshinttask.rb
@@ -52,7 +52,7 @@ module Jshintrb
     def define # :nodoc:
 
       actual_name = Hash === name ? name.keys.first : name
-      unless ::Rake.application.last_description
+      unless ::Rake.application.last_description || ::Rake.application.last_comment
         desc "Run JShint"
       end
       task name do
